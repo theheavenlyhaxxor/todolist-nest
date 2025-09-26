6098db06fb54ee039e5500e3ccb4eee2acb60f38
Jerome Infante <jeromeinfante.dev@gmail.com>
2025-09-26 09:49:34 +0800

    add: user RUD done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Patch, Get, Body, Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserUpdateDto } from './dto/user.update.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get('username')
+  async getUserByUsername(@Body('username') username: string) {
+    return this.userService.getUserByUsername(username);
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: number) {
+    return this.userService.getUser(id);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: number) {
+    return this.userService.deleteUser(id);
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: number,
+    @Body() userUpdateDto: UserUpdateDto,
+  ) {
+    return this.userService.updateUser(id, userUpdateDto);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { UserUpdateDto } from './dto/user.update.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly db: DatabaseService) {}
+
+  async getUser(id: number) {
+    const [rows] = await this.db
+      .getPool()
+      .execute('SELECT * FROM users WHERE id = ?', [id]);
+
+    const user = (rows as any[])[0];
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    return user;
+  }
+
+  async deleteUser(id: number) {
+    const [rows] = await this.db
+      .getPool()
+      .execute('DELETE FROM users WHERE id = ?', [id]);
+
+    const { affectedRows } = rows as any;
+
+    if (affectedRows === 0) {
+      throw new BadRequestException('User not found');
+    }
+
+    return { message: 'succesfully deleted user' };
+  }
+
+  async updateUser(id: number, userUpdateDto: UserUpdateDto) {
+    const { username, password } = userUpdateDto;
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const [rows] = await this.db
+      .getPool()
+      .execute('UPDATE users SET username = ?, password = ? WHERE id = ? ', [
+        username,
+        hash,
+        id,
+      ]);
+
+    const { affectedRows } = rows as any;
+
+    if (affectedRows === 0) {
+      throw new BadRequestException('User not found');
+    }
+
+    return { message: 'User Updated succesfully' };
+  }
+
+  async getAllUsers() {
+    const [rows] = await this.db.getPool().execute('SELECT * FROM users');
+    const data = rows as any[];
+    if (data.length === 0) {
+      throw new BadRequestException('No Data found');
+    }
+
+    return data;
+  }
+
+  async getUserByUsername(username: string) {
+    const [rows] = await this.db
+      .getPool()
+      .execute('SELECT * FROM users WHERE username = ?', [username]);
+
+    const affectedRows = (rows as any[])[0];
+
+    if (!affectedRows) {
+      throw new BadRequestException('User not found');
+    }
+
+    return affectedRows;
+  }
+}
