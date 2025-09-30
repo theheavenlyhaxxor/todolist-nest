f03ef67eb30566f3ad6cbb39ca2d70c11073cb4f
Jerome Infante <jeromeinfante.dev@gmail.com>
2025-09-30 21:57:55 +0800

    added api swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo List API')
+    .setDescription('The Todo List API descriptions')
+    .setVersion('1.0')
+    .addTag('todo')
+    .addBearerAuth(
+      // 👈 Add this for JWT
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token', // This is a name/key you will use in decorators
+    )
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
